3cc2eed27517e45864fb16ad7aa6b60e5ea96354
ghadakhamis <ghada.khamis.m@gmail.com>
2018-09-02 00:34:31 +0200

    change button name to edit

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -45,7 +45,7 @@
             <div class="form-group row mb-0">
                 <div class="col-md-6 offset-md-4">
                     <button type="submit" class="btn btn-primary">
-                         {{ __('Save') }}
+                         {{ __('Edit') }}
                     </button>
                 </div>
             </div>
